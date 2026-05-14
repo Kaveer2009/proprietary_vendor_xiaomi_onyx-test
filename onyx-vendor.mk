85a36dc18b465bc2d472190bc1d0469b50a4b3da
Zarathos30 <vale.rigni@gmail.com>
2026-05-14 15:05:40 +0200

    onyx: Enable CameraX extensions support
    Signed-off-by: Kaveer2009 <86919827+Kaveer2009@users.noreply.github.com>

--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -3241,6 +3241,7 @@ PRODUCT_PACKAGES += \
     uimgbalibrary \
     uimgbamanagerlibrary \
     uimservicelibrary \
+    camerax-vendor-extensions \
     qcrilhook \
     vendor.qti.hardware.alarm-V1.0-java \
     vendor.qti.ims.connection-V1.0-java \
